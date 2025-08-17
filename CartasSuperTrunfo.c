8e94b080ab0cf2a8d7e5ef515ec507b23a344a4b
castorSabio <pedrohenderson1994@outlook.com>
2025-08-17 13:35:42 -0300

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -8,18 +8,27 @@ struct CartaSuperTrunfo {
    char codigo[4]; // Supondo um comprimento máximo de 3 caracteres + ''
    char cidade[50];
    int populacao;
+   int pontosTuristicos;
+   float area;
+   float pib;
 };
 //Crie uma função para ler os dados das cartas: Esta função solicitará ao usuário as informações para cada carta.
 
 void lerCarta(struct CartaSuperTrunfo *carta) {
    printf("\nDigite o estado (uma letra de A a Z): ");
    scanf(" %c", &carta->estado);
-   printf("Digite o código da carta: ");
+   printf("\nDigite o código da carta: ");
    scanf(" %s", carta->codigo);
-   printf("Digite o nome da cidade: ");
+   printf("\nDigite o nome da cidade: ");
    scanf(" %s", carta->cidade);
-   printf("Digite a população da cidade: ");
+   printf("\nDigite a população da cidade: ");
    scanf(" %d", &carta->populacao);
+   printf("\nDigite os pontosTuristicos");
+   scanf("%d", &carta->pontosTuristicos);
+   printf("\nDigite a area em km²: ");
+   scanf("%f", &carta->area);
+   printf("\nDigite o pib: ");
+   scanf("%f", &carta->pib);
 }
 //Implemente a função principal: Aqui chamaremos a função para ler os dados das duas cartas e depois exibiremos essas informações para o usuário.
 
